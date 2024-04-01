4f48cf78ee7fe858aeeaf85262ce3f8f68c2fe82
hidaya <hidaya.regragui@gmail.com>
2024-04-01 21:52:56 +0000

    task11

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,9 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 
+
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
