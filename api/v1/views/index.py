aa5c2e0e727d8c0d06d2d054a09221369b65a37c
hidaya <hidaya.regragui@gmail.com>
2024-04-01 12:54:27 +0000

    task4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,28 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Return the status of the API."""
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Retrieve the number of each object type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
