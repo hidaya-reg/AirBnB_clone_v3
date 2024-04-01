f88d693fbf020a35f68d26bd7e5c16f77f542f28
hidaya <hidaya.regragui@gmail.com>
2024-04-01 13:01:18 +0000

    task5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """create api"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,6 +16,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Close the current SQLAlchemy session."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
