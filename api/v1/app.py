a3566ae8438b55946fbaf0f691eedbd122dbfa3e
hidaya <hidaya.regragui@gmail.com>
2024-04-01 21:55:22 +0000

    task12

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """create api"""
 
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
